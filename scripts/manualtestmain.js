a848e51471d55495a241219d15a0c74a03c3bb09
Tyler Heck <tyler.heck@gmail.com>
2014-06-14 21:40:43 -0400

    add buttons to manual test page

--- scripts/manualtestmain.js
+++ scripts/manualtestmain.js
@@ -5,12 +5,18 @@ require.config({
 });
 
 
-require(['palette'], function(Palette) {
-  window.testPalette = new Palette({
-    domID: '#test-palette',
-    dimensions: { width: 200, height: 100 },
-    anchorPosition: { x: 300, y: 300 },
-    anchorEdge: Palette.ANCHOR_EDGES.LEFT,
-    isVisible: true
-  });
+require(['jquery', 'palette', 'button'], function($, Palette, Buttons) {
+	$(function () {
+		window.testPalette = new Palette({
+			domID: '#test-palette',
+			dimensions: { width: 200, height: 100 },
+			anchorPosition: { x: 300, y: 300 },
+			anchorEdge: Palette.ANCHOR_EDGES.LEFT,
+			isVisible: true,
+			anchorEdgeBounds: { min: 260, max: Infinity }
+		});
+
+    $('.dk-button').width(100).height(50);
+    $('.dk-toggleable-button').width(100).height(50);
+	});
 });
