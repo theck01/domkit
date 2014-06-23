e7176348f9f7816623651ec19f9f859a9cf64a43
Tyler Heck <tyler.heck@gmail.com>
2014-06-22 20:08:53 -0400

    add test radio group to manual test page

--- scripts/manualtestmain.js
+++ scripts/manualtestmain.js
@@ -5,27 +5,57 @@ require.config({
 });
 
 
-require(['jquery', 'ui/palette', 'ui/button'], function($, Palette, Button) {
+require(['jquery', 'controllers/radiogroup', 'ui/palette', 'ui/button'],
+    function($, RadioGroup, Palette, Button) {
 	$(function () {
-    var testPalette = null;
-
     var regularButton = Button.create('#test-button');
     regularButton.addClickHandler(function () {
-      console.log('Clicked.');
+      console.log('Button clicked.');
     });
 
-    var toggleButton = Button.create('#test-toggle');
-    toggleButton.addClickHandler(function (toggleState) {
-      if (toggleState) console.log('Toggle active.');
-      else console.log('Toggle disabled.');
-      testPalette.visible(toggleState);
+    var testPalettes = [];
+    var radioButtons = [];
+
+    radioButtons[0] = Button.create('#test-radio-0');
+    radioButtons[0].addClickHandler(function (toggleState) {
+      if (toggleState) console.log('Radio 0 active.');
+      else console.log('Radio 0 disabled.');
+      testPalettes[0].visible(toggleState);
     });
+    radioButtons[1] = Button.create('#test-radio-1');
+    radioButtons[1].addClickHandler(function (toggleState) {
+      if (toggleState) console.log('Radio 1 active.');
+      else console.log('Radio 1 disabled.');
+      testPalettes[1].visible(toggleState);
+    });
+    radioButtons[2] = Button.create('#test-radio-2');
+    radioButtons[2].addClickHandler(function (toggleState) {
+      if (toggleState) console.log('Radio 2 active.');
+      else console.log('Radio 2 disabled.');
+      testPalettes[2].visible(toggleState);
+    });
+
+    new RadioGroup(radioButtons);
 
     $('.dk-button').width(100).height(50);
     $('.dk-toggleable-button').width(100).height(50);
 
-		testPalette = new Palette({
-			domID: '#test-toggle',
+		testPalettes[0] = new Palette({
+			domID: '#test-radio-0',
+			dimensions: { width: 200, height: 100 },
+			anchorEdge: Palette.ANCHOR_EDGES.BOTTOM,
+			isVisible: false,
+			anchorEdgeBounds: { min: 0, max: Infinity }
+		});
+		testPalettes[1] = new Palette({
+			domID: '#test-radio-1',
+			dimensions: { width: 200, height: 100 },
+			anchorEdge: Palette.ANCHOR_EDGES.BOTTOM,
+			isVisible: false,
+			anchorEdgeBounds: { min: 0, max: Infinity }
+		});
+		testPalettes[2] = new Palette({
+			domID: '#test-radio-2',
 			dimensions: { width: 200, height: 100 },
 			anchorEdge: Palette.ANCHOR_EDGES.BOTTOM,
 			isVisible: false,
