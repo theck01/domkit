a31acb8159833c9bd8b6daef695c21ceccda9458
Tyler Heck <tyler.heck@gmail.com>
2014-06-22 20:08:39 -0400

    add RadioGroup controller

---
+++ scripts/controllers/radiogroup.js
@@ -0,0 +1,106 @@
+define([], function () {
+  // A toggle radio group controls a group of toggleable elements, allowing
+  // only one of the group of the elements to be active at any one time. Resets
+  // all elements toggle state to false on creation.
+  // Arguments:
+  //   elements: An array of toggleable element instances.
+  //   opt_activeIndex: The index of the element within the elements array that
+  //       should start as toggled.
+  var RadioGroup = function (elements, opt_activeIndex) {
+    this._activeElement = opt_activeIndex === undefined ?
+        null : elements[opt_activeIndex];
+    this._toggleableElements = [];
+    this._toggleHandlers = this._generateHandlers(elements.length);
+
+    for (var i = 0; i < elements.length; i++) {
+      this._assertElementIsToggleable(elements[i]);
+
+      if (elements[i].getState() && i !== opt_activeIndex) {
+        elements[i].setState(false);
+      }
+      this._toggleableElements.push(elements[i]);
+      this._toggleableElements[i].addStateHandler(this._toggleHandlers[i]);
+    }
+
+    if (opt_activeIndex !== undefined &&
+        !this._toggleableElements[opt_activeIndex].getState()) {
+      this._toggleableElements[opt_activeIndex].setState(true);
+    }
+  };
+
+
+  // Asserts that the element in question is a toggleable element.
+  // Arguments:
+  //   element: The element to assert is toggleable.
+  RadioGroup.prototype._assertElementIsToggleable = function (element) {
+    if (!element.isToggleable || !element.isToggleable() ||
+        !element.getState || !element.setState || !element.addStateHandler ||
+        !element.removeStateHandler) {
+      throw Error(
+          'Cannot add non togglable element to a toggle radio group. A ' +
+          'toggleable element has an isToggleable method which returns ' +
+          'true, a getState method, a setState method, an addStateHandler, ' +
+          'method, and a removeStateHandler method.');
+    }
+  };
+
+
+  // destroy the radio group, removing all handers from the elements
+  RadioGroup.prototype.destroy = function () {
+    for (var i = 0; i < this._toggeableElements; i++) {
+      this._toggleableElements[i].removeStateHandler(
+          this._toggleHandlers[i]);
+    }
+
+    this._toggleHandlers = [];
+    this._toggeableElements = [];
+    this._activeElement = null;
+  };
+
+
+  // _generateHandlers creates the given number of state change handlers.
+  // Arguments:
+  //   numHandlers: The number of toggle handlers to generate
+  // Returns and array of handler functions, each taking a boolean value for
+  // the state of the assoicated element.
+  RadioGroup.prototype._generateHandlers = function (numElements) {
+    var toggleHandlers = [];
+    var stateChangeHandler = this._handleStateChange.bind(this);
+
+    for (var i = 0; i < numElements; i++) {
+      var handler = (function (elementIndex) {
+        return stateChangeHandler.bind(stateChangeHandler, elementIndex);
+      })(i);
+
+      toggleHandlers.push(handler);
+    }
+
+    return toggleHandlers;
+  };
+
+
+  // getActiveElement returns the element currently active within the radio
+  // group, or null if no element is active.
+  RadioGroup.prototype._getActiveElement = function () {
+    return this._activeElement;
+  };
+
+
+  // _handleStateChange of the element at the given index.
+  // Arguments:
+  //   elementIndex: The index of the element that changed state.
+  //   state: The state of the element after the change.
+  RadioGroup.prototype._handleStateChange = function (elementIndex, state) {
+    if (!state) return;
+
+    this._activeElement = this._toggleableElements[elementIndex];
+
+    for (var i = 0; i < this._toggleableElements.length; i++) {
+      if (i === elementIndex) continue;
+      this._toggleableElements[i].setState(false);
+    }
+  };
+  
+
+  return RadioGroup;
+});
